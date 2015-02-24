6d8db76adb338c93b4c78cd108e01ae96f11eab3
lalyos <lalyos@yahoo.com>
2015-02-24 22:47:13 +0100

    clean up of curl/sleep health check workarounds

--- start-cb.sh
+++ start-cb.sh
@@ -220,7 +220,6 @@ start_cloudbreak_db() {
       postgres:$DOCKER_TAG_POSTGRES
 
     wait_for_service cbdb
-    sleep 20
 }
 
 start_uaa() {
@@ -233,10 +232,7 @@ start_uaa() {
       -v /var/lib/cloudbreak/uaadb:/var/lib/postgresql/data \
       postgres:$DOCKER_TAG_POSTGRES
 
-    debug "waits for uaadb get registered in consul"
     wait_for_service uaadb
-    sleep 20
-    debug "uaa db: $(dhp uaadb) "
 
     docker run -d -P \
       --name="uaa" \
@@ -248,9 +244,7 @@ start_uaa() {
       -p 8089:8080 \
       sequenceiq/uaa:$DOCKER_TAG_UAA
 
-    local uaaaddress=$(docker inspect -f "{{.NetworkSettings.IPAddress}}" uaa):8080/info
-    debug $uaaaddress
-    checkHealthOnUrl $uaaaddress
+    wait_for_service uaa
 }
 
 start_cloudbreak_shell() {
@@ -279,8 +273,7 @@ start_cloudbreak() {
 
     debug $desc
     wait_for_service cbdb
-    debug "cloudbreak db: $(dhp cbdb)"
-    export CB_HOST_ADDR=$CLOUDBREAK_PUBLIC_HOST_ADDRESS
+    export CB_HOST_ADDR=http://$(dh consul):8080
     cb_envs_to_docker_options
 
     docker run -d \
@@ -298,9 +291,7 @@ start_cloudbreak() {
         -p 8080:8080 \
         sequenceiq/cloudbreak:$DOCKER_TAG_CLOUDBREAK bash
 
-    local cbaddress=$(docker inspect -f "{{.NetworkSettings.IPAddress}}" cloudbreak):8080/info
-    debug $cbaddress
-    checkHealthOnUrl $cbaddress
+    wait_for_service cloudbreak
 }
 
 start_uluwatu() {
@@ -399,39 +390,6 @@ bridge_osx() {
     BRIDGE_IP=$(docker run --rm mini/base ip ro | grep default | cut -d" " -f 3)
     sudo networksetup -setdnsservers Wi-Fi 192.168.1.1 $BRIDGE_IP 8.8.8.8
     sudo networksetup -setsearchdomains Wi-Fi service.consul node.consul
-}
-
-checkHealthOnUrl() {
-  declare url=$1
-  declare maxAttempts=10
-  declare pollTimeout=30
-
-  cat <<EOF
-========================================================
-= check service availabilty =
-= by checking the health url:
-=   $url
-=
-= maxAttempts=$maxAttempts
-========================================================
-EOF
-
-  for (( i=1; i<=$maxAttempts; i++ ))
-  do
-      echo "GET $url. Attempt #$i"
-      code=`curl -sL -w "%{http_code}\\n" "$url" -o /dev/null`
-      echo "Found code $code"
-      if [ "x$code" = "x200" ]
-      then
-           echo "Service on '"$url"' is available!"
-           break
-      elif [ $i -eq $maxAttempts ]
-      then
-           echo "Service on '"$url"' not started in time."
-           exit 1
-      fi
-      sleep $pollTimeout
-  done
 }
 
 main() {
