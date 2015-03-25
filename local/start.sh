c96719ff40ab7f242cdbbe79f78df1e4a9e5a346
Richard Doktorics <richard.doktorics@sequenceiq.com>
2015-03-25 15:59:13 +0100

    Update start.sh

--- local/start.sh
+++ local/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash +x
 
 export HOST_ADDRESS=http://localhost
+# The public IP of the machine and the port 8080 so the pattern is: http://xxx.xxx.xxx.xxx:8080
 export CLOUDBREAK_PUBLIC_HOST_ADDRESS=""
 
 $(dirname $BASH_SOURCE)/../start-cb.sh
