bad871580e9f1bfb27059cb5ac75424550de8dcf
lalyos <lalyos@yahoo.com>
2015-04-14 07:18:54 +0200

    moove to cbd

--- local/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash +x
-
-export HOST_ADDRESS=http://localhost
-# The public IP of the machine and the port 8080 so the pattern is: http://xxx.xxx.xxx.xxx:8080
-export CLOUDBREAK_PUBLIC_HOST_ADDRESS=""
-
-$(dirname $BASH_SOURCE)/../start-cb.sh
